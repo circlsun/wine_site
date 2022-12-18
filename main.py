561102909acc33729dee1ba125895fb731735675
Matveev Andrey <circlsun@gmail.com>
2022-12-18 12:29:04 +0300

    Add main()

--- main.py
+++ main.py
@@ -5,33 +5,38 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from get_age import get_age
 
 
-year_foundation = 1920
+def main():
+    year_foundation = 1920
 
-wines = pandas.read_excel(
-    'wine3.xlsx',
-    usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
-    na_values='None',
-    keep_default_na=False
-    ).to_dict('records')
+    wines = pandas.read_excel(
+        'wine3.xlsx',
+        usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
+        na_values='None',
+        keep_default_na=False
+        ).to_dict('records')
 
-product_line = defaultdict(list)
-for wine in wines:
-    product_line[wine.get('Категория')].append(wine)
+    product_line = defaultdict(list)
+    for wine in wines:
+        product_line[wine.get('Категория')].append(wine)
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-rendered_page = template.render(
-    age_winery=get_age(year_foundation),
-    wines=product_line,
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
     )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    template = env.get_template('template.html')
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    rendered_page = template.render(
+        age_winery=get_age(year_foundation),
+        wines=product_line,
+        )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
